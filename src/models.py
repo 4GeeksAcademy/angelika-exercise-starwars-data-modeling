60da6a475452f160b81c8cb2dc38a2d6724efcef
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-07-01 19:08:00 +0000

    modificacion

--- src/models.py
+++ src/models.py
@@ -1,169 +1,188 @@
+import datetime
 import os
-import sys
-from xmlrpc.client import DateTime
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Date
+from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
+from eralchemy import render_er
 
-Base = declarative_base()
+from flask_sqlalchemy import SQLAlchemy
 
-people_films = Table('people_films', Base.metadata,
-    Column('people_id', String, ForeignKey('people.url'), primary_key=True),
-    Column('film_id', String, ForeignKey('films.url'), primary_key=True)
-)
+db = SQLAlchemy()
 
-starships_films = Table('starships_films', Base.metadata,
-    Column('starship_id', String, ForeignKey('starships.url'), primary_key=True),
-    Column('film_id', String, ForeignKey('films.url'), primary_key=True)
-)
+class PeopleFilms(db.Model):
+    __tablename__ = 'people_films'
+    people_id = db.Column(db.String, db.ForeignKey('people.url'), primary_key=True)
+    film_id = db.Column(db.String, db.ForeignKey('films.url'), primary_key=True)
 
-people_starships = Table('people_starships', Base.metadata,
-    Column('people_id', String, ForeignKey('people.url'), primary_key=True),
-    Column('starship_id', String, ForeignKey('starships.url'), primary_key=True)
-)
+class StarshipsFilms(db.Model):
+    __tablename__ = 'starships_films'
+    starship_id = db.Column(db.String, db.ForeignKey('starships.url'), primary_key=True)
+    film_id = db.Column(db.String, db.ForeignKey('films.url'), primary_key=True)
 
-vehicles_films = Table('vehicles_films', Base.metadata,
-    Column('vehicle_id', String, ForeignKey('vehicles.url'), primary_key=True),
-    Column('film_id', String, ForeignKey('films.url'), primary_key=True)
-)
+class PeopleStarships(db.Model):
+    __tablename__ = 'people_starships'
+    people_id = db.Column(db.String, db.ForeignKey('people.url'), primary_key=True)
+    starship_id = db.Column(db.String, db.ForeignKey('starships.url'), primary_key=True)
 
-people_vehicles = Table('people_vehicles', Base.metadata,
-    Column('people_id', String, ForeignKey('people.url'), primary_key=True),
-    Column('vehicle_id', String, ForeignKey('vehicles.url'), primary_key=True)
-)
+class VehiclesFilms(db.Model):
+    __tablename__ = 'vehicles_films'
+    vehicle_id = db.Column(db.String, db.ForeignKey('vehicles.url'), primary_key=True)
+    film_id = db.Column(db.String, db.ForeignKey('films.url'), primary_key=True)
 
-species_films = Table('species_films', Base.metadata,
-    Column('species_id', String, ForeignKey('species.url'), primary_key=True),
-    Column('film_id', String, ForeignKey('films.url'), primary_key=True)
-)
+class PeopleVehicles(db.Model):
+    __tablename__ = 'people_vehicles'
+    people_id = db.Column(db.String, db.ForeignKey('people.url'), primary_key=True)
+    vehicle_id = db.Column(db.String, db.ForeignKey('vehicles.url'), primary_key=True)
 
-species_people = Table('species_people', Base.metadata,
-    Column('species_id', String, ForeignKey('species.url'), primary_key=True),
-    Column('people_id', String, ForeignKey('people.url'), primary_key=True)
-)
+class SpeciesFilms(db.Model):
+    __tablename__ = 'species_films'
+    species_id = db.Column(db.String, db.ForeignKey('species.url'), primary_key=True)
+    film_id = db.Column(db.String, db.ForeignKey('films.url'), primary_key=True)
 
-planets_films = Table('planets_films', Base.metadata,
-    Column('planet_id', String, ForeignKey('planets.url'), primary_key=True),
-    Column('film_id', String, ForeignKey('films.url'), primary_key=True)
-)
+class SpeciesPeople(db.Model):
+    __tablename__ = 'species_people'
+    species_id = db.Column(db.String, db.ForeignKey('species.url'), primary_key=True)
+    people_id = db.Column(db.String, db.ForeignKey('people.url'), primary_key=True)
 
-planets_people = Table('planets_people', Base.metadata,
-    Column('planet_id', String, ForeignKey('planets.url'), primary_key=True),
-    Column('people_id', String, ForeignKey('people.url'), primary_key=True)
-)
+class PlanetsFilms(db.Model):
+    __tablename__ = 'planets_films'
+    planet_id = db.Column(db.String, db.ForeignKey('planets.url'), primary_key=True)
+    film_id = db.Column(db.String, db.ForeignKey('films.url'), primary_key=True)
 
-class People(Base):
+class PlanetsPeople(db.Model):
+    __tablename__ = 'planets_people'
+    planet_id = db.Column(db.String, db.ForeignKey('planets.url'), primary_key=True)
+    people_id = db.Column(db.String, db.ForeignKey('people.url'), primary_key=True)
+
+class People(db.Model):
     __tablename__ = 'people'
-    name = Column(String, nullable=False)
-    birth_year = Column(String)
-    eye_color = Column(String)
-    gender = Column(String)
-    hair_color = Column(String)
-    height = Column(String)
-    mass = Column(String)
-    skin_color = Column(String)
-    homeworld = Column(String)
-    url = Column(String, primary_key=True)
-    created = Column(DateTime)
-    edited = Column(DateTime)
-    films = relationship('Film', secondary=people_films, back_populates='characters')
-    starships = relationship('Starship', secondary=people_starships, back_populates='pilots')
-    vehicles = relationship('Vehicle', secondary=people_vehicles, back_populates='pilots')
-    species = relationship('Species', secondary=species_people, back_populates='people')
-    planets = relationship('Planet', secondary=planets_people, back_populates='residents')
+    url = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    birth_year = db.Column(db.String)
+    eye_color = db.Column(db.String)
+    gender = db.Column(db.String)
+    hair_color = db.Column(db.String)
+    height = db.Column(db.String)
+    mass = db.Column(db.String)
+    skin_color = db.Column(db.String)
+    homeworld = db.Column(db.String)
+    created = db.Column(DateTime, default=datetime.datetime.utcnow)
+    edited = db.Column(DateTime, default=datetime.datetime.utcnow)
 
-class Film(Base):
+    films = db.relationship('Film', secondary='people_films', backref=db.backref('characters', lazy='dynamic'))
+    starships = db.relationship('Starship', secondary='starships_films', backref=db.backref('pilots', lazy='dynamic'))
+    vehicles = db.relationship('Vehicle', secondary='vehicles_films', backref=db.backref('pilots', lazy='dynamic'))
+    species = db.relationship('Species', secondary='species_people', backref=db.backref('people', lazy='dynamic'))
+    planets = db.relationship('Planet', secondary='planets_people', backref=db.backref('residents', lazy='dynamic'))
+
+    def to_dict(self):
+        return {
+            'url': self.url,
+            'name': self.name,
+            'birth_year': self.birth_year,
+            'eye_color': self.eye_color,
+            'gender': self.gender,
+            'hair_color': self.hair_color,
+            'height': self.height,
+            'mass': self.mass,
+            'skin_color': self.skin_color,
+            'homeworld': self.homeworld,
+            'created': self.created,
+            'edited': self.edited,
+        }
+
+class Film(db.Model):
     __tablename__ = 'films'
-    title = Column(String, nullable=False)
-    episode_id = Column(Integer, nullable=False)
-    opening_crawl = Column(String)
-    director = Column(String)
-    producer = Column(String)
-    release_date = Column(Date)
-    species = relationship('Species', secondary=species_films, back_populates='films')
-    starships = relationship('Starship', secondary=starships_films, back_populates='films')
-    vehicles = relationship('Vehicle', secondary=vehicles_films, back_populates='films')
-    characters = relationship('People', secondary=people_films, back_populates='films')
-    planets = relationship('Planet', secondary=planets_films, back_populates='films')
-    url = Column(String, primary_key=True)
-    created = Column(DateTime)
-    edited = Column(DateTime)
+    url = db.Column(db.String, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    episode_id = db.Column(db.Integer, nullable=False)
+    opening_crawl = db.Column(db.String)
+    director = db.Column(db.String)
+    producer = db.Column(db.String)
+    release_date = db.Column(Date)
+    created = db.Column(DateTime, default=datetime.datetime.utcnow)
+    edited = db.Column(DateTime, default=datetime.datetime.utcnow)
 
-class Starship(Base):
+    species = db.relationship('Species', secondary='species_films', backref=db.backref('films', lazy='dynamic'))
+    starships = db.relationship('Starship', secondary='starships_films', backref=db.backref('films', lazy='dynamic'))
+    vehicles = db.relationship('Vehicle', secondary='vehicles_films', backref=db.backref('films', lazy='dynamic'))
+    planets = db.relationship('Planet', secondary='planets_films', backref=db.backref('films', lazy='dynamic'))
+
+    def to_dict(self):
+        return {
+            'url': self.url,
+            'title': self.title,
+            'episode_id': self.episode_id,
+            'opening_crawl': self.opening_crawl,
+            'director': self.director,
+            'producer': self.producer,
+            'release_date': self.release_date,
+            'created': self.created,
+            'edited': self.edited,
+        }
+
+class Starship(db.Model):
     __tablename__ = 'starships'
-    name = Column(String, nullable=False)
-    model = Column(String)
-    starship_class = Column(String)
-    manufacturer = Column(String)
-    cost_in_credits = Column(String)
-    length = Column(String)
-    crew = Column(String)
-    passengers = Column(String)
-    max_atmosphering_speed = Column(String)
-    hyperdrive_rating = Column(String)
-    MGLT = Column(String)
-    cargo_capacity = Column(String)
-    consumables = Column(String)
-    films = relationship('Film', secondary=starships_films, back_populates='starships')
-    pilots = relationship('People', secondary=people_starships, back_populates='starships')
-    url = Column(String, primary_key=True)
-    created = Column(DateTime)
-    edited = Column(DateTime)
+    url = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    model = db.Column(db.String)
+    starship_class = db.Column(db.String)
+    manufacturer = db.Column(db.String)
+    cost_in_credits = db.Column(db.String)
+    length = db.Column(db.String)
+    crew = db.Column(db.String)
+    passengers = db.Column(db.String)
+    max_atmosphering_speed = db.Column(db.String)
+    hyperdrive_rating = db.Column(db.String)
+    MGLT = db.Column(db.String)
+    cargo_capacity = db.Column(db.String)
+    consumables = db.Column(db.String)
+    created = db.Column(DateTime, default=datetime.datetime.utcnow)
+    edited = db.Column(DateTime, default=datetime.datetime.utcnow)
 
-class Vehicle(Base):
+    films = db.relationship('Film', secondary='starships_films', backref=db.backref('starships', lazy='dynamic'))
+    pilots = db.relationship('People', secondary='people_starships', backref=db.backref('starships', lazy='dynamic'))
+
+    def to_dict(self):
+        return {
+            'url': self.url,
+            'name': self.name,
+            'model': self.model,
+            'starship_class': self.starship_class,
+            'manufacturer': self.manufacturer,
+            'cost_in_credits': self.cost_in_credits,
+            'length': self.length,
+            'crew': self.crew,
+            'passengers': self.passengers,
+            'max_atmosphering_speed': self.max_atmosphering_speed,
+            'hyperdrive_rating': self.hyperdrive_rating,
+            'MGLT': self.MGLT,
+            'cargo_capacity': self.cargo_capacity,
+            'consumables': self.consumables,
+            'created': self.created,
+            'edited': self.edited,
+        }
+
+class Vehicle(db.Model):
     __tablename__ = 'vehicles'
-    name = Column(String, nullable=False)
-    model = Column(String)
-    vehicle_class = Column(String)
-    manufacturer = Column(String)
-    length = Column(String)
-    cost_in_credits = Column(String)
-    crew = Column(String)
-    passengers = Column(String)
-    max_atmosphering_speed = Column(String)
-    cargo_capacity = Column(String)
-    consumables = Column(String)
-    films = relationship('Film', secondary=vehicles_films, back_populates='vehicles')
-    pilots = relationship('People', secondary=people_vehicles, back_populates='vehicles')
-    url = Column(String, primary_key=True)
-    created = Column(DateTime)
-    edited = Column(DateTime)
+    url = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    model = db.Column(db.String)
+    vehicle_class = db.Column(db.String)
+    manufacturer = db.Column(db.String)
+    length = db.Column(db.String)
+    cost_in_credits = db.Column(db.String)
+    crew = db.Column(db.String)
+    passengers = db.Column(db.String)
+    max_atmosphering_speed = db.Column(db.String)
+    cargo_capacity = db.Column(db.String)
+    consumables = db.Column(db.String)
+    created = db.Column(DateTime, default=datetime.datetime.utcnow)
+    edited = db.Column(DateTime, default=datetime.datetime.utcnow)
 
-class Species(Base):
-    __tablename__ = 'species'
-    name = Column(String, nullable=False)
-    classification = Column(String)
-    designation = Column(String)
-    average_height = Column(String)
-    average_lifespan = Column(String)
-    eye_colors = Column(String)
-    hair_colors = Column(String)
-    skin_colors = Column(String)
-    language = Column(String)
-    homeworld = Column(String)
-    people = relationship('People', secondary=species_people, back_populates='species')
-    films = relationship('Film', secondary=species_films, back_populates='species')
-    url = Column(String, primary_key=True)
-    created = Column(DateTime)
-    edited = Column(DateTime)
+    films = db.relationship('Film', secondary='vehicles_films', backref=db.backref('vehicles', lazy='dynamic'))
+    pilots = db.relationship('People', secondary='people_vehicles', backref=db.backref('vehicles', lazy='dynamic'))
 
-class Planet(Base):
-    __tablename__ = 'planets'
-    name = Column(String, nullable=False)
-    diameter = Column(String)
-    rotation_period = Column(String)
-    orbital_period = Column(String)
-    gravity = Column(String)
-    population = Column(String)
-    climate = Column(String)
-    terrain = Column(String)
-    surface_water = Column(String)
-    residents = relationship('People', secondary=planets_people, back_populates='planets')
-    films = relationship('Film', secondary=planets_films, back_populates='planets')
-    url = Column(String, primary_key=True)
-    created = Column(DateTime)
-    edited = Column(DateTime)
 
     def to_dict(self):
         return {}
